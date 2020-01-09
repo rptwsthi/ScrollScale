063f9e778b2e036c0a81330863b43dd709e9e2a8
Arpit <Arpit@technologies33.com>
2020-01-09 16:22:55 +0530

    - added table view with all code to pick and display the information - added code to rotate picker - added code to Ui and interval for selection
    Now going t to remove section header and managing all information using cell

--- ScrollScale/SSView.swift
+++ ScrollScale/SSView.swift
@@ -15,31 +15,268 @@ extension NSObject {
     }
 }
 
-open class RPTScaleView : UIView, UIPickerViewDelegate, UIPickerViewDataSource  {
-    //MARK: IBInspectable
-    @IBInspectable public var scale : String = "cm"
-    @IBInspectable public var interval: Int = 10
-    @IBInspectable public var color : UIColor = .green
-    @IBInspectable public var minValue = 0
-    @IBInspectable public var maxValue = 1000
-    @IBInspectable  public var isVertical: Bool = true {
+extension UIView {
+   /**
+     Rotate a view by specified degrees
+     - parameter angle: angle in degrees
+     */
+    func rotate(angle: CGFloat) {
+        let radians = angle / 180.0 * CGFloat.pi
+        let rotation = self.transform.rotated(by: radians);
+        //self.transform.
+        self.transform = rotation
+    }
+}
+
+//MARK:- Arrow
+enum PointerDirection {
+    case left
+    case down
+    case right
+    case up
+}
+
+class Arrow: UIView {
+    let shapeLayer = CAShapeLayer()
+    var arrowColor:UIColor = .black {
         didSet{
-            rotateScale()
+            shapeLayer.fillColor = arrowColor.cgColor
         }
     }
     
-    func rotateScale() {
-        
-    }
-
-    //..
-    var indicator : Indicator!
-    @IBInspectable public var arrowSize: CGFloat = 15 {
+    var pointerDirection: PointerDirection = .down {
         didSet{
-            let center = indicator.superview!.center
-            indicator.frame = CGRect(x: center.x - arrowSize/2, y: center.y - arrowSize/2, width: arrowSize, height: arrowSize)
+            switch pointerDirection {
+            case .left:
+                self.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+                break
+
+            case .down:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2)
+                break
+
+            case .right:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+                break
+
+            case .up:
+                self.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                break
+            }
         }
     }
+
+    init(origin: CGPoint, size: CGFloat ) {
+        super.init(frame: CGRect(x: origin.x, y: origin.y, width: size, height: size))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(_ rect: CGRect) {
+
+        // Get size
+        let size = self.layer.frame.width
+
+        // Create path
+        let bezierPath = UIBezierPath()
+
+        // Draw points
+        let qSize = size/4
+
+        bezierPath.move(to: CGPoint(x: 0, y: qSize))
+        bezierPath.addLine(to: CGPoint(x: size, y: qSize))
+        bezierPath.addLine(to: CGPoint(x: size/2, y: qSize*3))
+        bezierPath.addLine(to: CGPoint(x: 0, y: qSize))
+        bezierPath.close()
+
+        // Mask to path
+        shapeLayer.path = bezierPath.cgPath
+      //  shapeLayer.fillColor = arrowColor.cgColor
+       
+        if #available(iOS 12.0, *) {
+            self.layer.addSublayer (shapeLayer)
+        } else {
+            self.layer.mask = shapeLayer
+        }
+    }
+}
+
+
+//MARK:-
+class ScaleBarCell : UITableViewCell {
+    var indicatorView = UIView()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(indicatorView)
+        self.selectionStyle = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        //..
+        self.contentView.addSubview(indicatorView)
+        self.selectionStyle = .none
+    }
+}
+
+//MARK:-
+class ScaleBarHeader : UITableViewHeaderFooterView {
+    let label = UILabel()
+    let indicatorView = UIView()
+    var setHorizontal : Bool = true {
+        didSet {
+            self.label.transform = CGAffineTransform(scaleX: -1, y: 1);
+        }
+    }
+    
+    var font : UIFont! {
+        didSet {
+            self.label.font = self.font
+        }
+    }
+    
+    var textColor : UIColor? {
+        didSet {
+            self.label.textColor = self.textColor
+        }
+    }
+
+    var indicatorColor : UIColor = .gray {
+        didSet {
+            self.indicatorView.backgroundColor = self.indicatorColor
+        }
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            setupUI()
+        }
+    }
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        //
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.75
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setupUI() {
+        var f = self.frame
+        f.origin = .zero
+        f.size.height = 2
+        indicatorView.frame = f
+        self.addSubview(indicatorView)
+        
+        f = self.frame
+        f.size.height -= 6
+        f.origin.y = 6
+        label.frame = f
+        self.addSubview(label)
+    }
+}
+
+//..
+class ScalePointer : UIView {
+    let valueLabel = UILabel()
+    let scArrow = Arrow(origin: .zero, size: 12)
+    let scScaleLable = UILabel()
+    let scLineView = UIView()
+    var color : UIColor! = .blue {
+        didSet {
+            scLineView.backgroundColor = color
+            scScaleLable.textColor = color
+            valueLabel.textColor = color
+            scArrow.arrowColor = color
+        }
+    }
+    
+    func rotateSubViews (angle:CGFloat) {
+        self.scScaleLable.rotate(angle:angle)
+        self.valueLabel.rotate(angle:angle)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //
+        setupUI()
+        
+        valueLabel.adjustsFontSizeToFitWidth = true
+        valueLabel.minimumScaleFactor = 0.5
+        scScaleLable.adjustsFontSizeToFitWidth = true
+        scScaleLable.minimumScaleFactor = 0.5
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        //..
+        setupUI()
+    }
+    
+    /**
+            Function is called tight after view s initialized and then set up UI for system.
+     */
+    func setupUI () {
+        var f = self.frame
+        f.origin.y = 0
+        f.size.width = 72
+        f.origin.x = 64
+        valueLabel.frame = f
+        valueLabel.textAlignment = .center
+        //valueLabel.backgroundColor = UIColor(white: 0.6, alpha: 0.4)
+        self.addSubview(valueLabel)
+        
+        f.origin.x += f.size.width + 12
+        f.origin.y = f.size.height / 2 - scArrow.frame.size.height / 2
+        f.size = scArrow.frame.size
+        scArrow.frame = f
+        scArrow.pointerDirection = .right
+        self.addSubview(scArrow)
+        
+        f.origin.x += f.size.width + 16
+        f.size = self.frame.size
+        f.size.width = 36
+        f.origin.y = 0
+        scScaleLable.frame = f
+        scScaleLable.textAlignment = .center
+        self.addSubview(scScaleLable)
+        
+        f.origin.x += f.size.width + 16
+        f.size.width = self.frame.size.width - f.origin.x
+        f.size.height = 2
+        f.origin.y = self.frame.size.height / 2 - f.size.height / 2
+        scLineView.frame = f
+        self.addSubview(scLineView)
+    }
+}
+
+open class RPTScaleView : UIView, UITableViewDelegate, UITableViewDataSource  {
+    //MARK: IBInspectable
+    @IBInspectable public var scale : String = "cm"
+    @IBInspectable public var font : UIFont = UIFont.systemFont(ofSize: 14, weight: .ultraLight)
+    @IBInspectable public var textColor : UIColor = .green
+    @IBInspectable public var color : UIColor = .green
+    @IBInspectable public var selectionColor : UIColor = .blue
+    
+    @IBInspectable public var interval: Int = 10
+    @IBInspectable public var range : NSRange = NSMakeRange(0, 1000)
+    @IBInspectable  public var setHorizontal: Bool = true {
+        didSet{
+            self.rotate(angle: (setHorizontal) ? 90 : 0)
+        }
+    }
+    
+    var table : UITableView!
+    var pointer : ScalePointer!
     
     // Init
     public override init(frame: CGRect) {
@@ -58,113 +295,144 @@ open class RPTScaleView : UIView, UIPickerViewDelegate, UIPickerViewDataSource  
     }
     
     func setupUI()  {
-        //addScalePicker
-        addScalePicker()
+        //addScaleTable
+        addScaleTable()
+        
+        //..
+        addIndicatorArrow()
     }
     
     //UITableViewDelegate, UITableViewDataSource
-    func addScalePicker () {
+    func addScaleTable () {
         var f = self.frame
-        let h = f.size.height / 3
+
+        /*let h = f.size.height / 3
         f.origin.y -= h / 2
-        f.size.height += h
+        f.size.height += h*/
         
-        var w = f.size.width / 6 
+        var w = f.size.width / 6
         w = (w > 60) ? w : 60
         f.origin.x = f.size.width - w
         f.size.width = w
-        let picker = UIPickerView(frame: f)
-        picker.delegate = self
-        picker.dataSource = self
-        self.addSubview(picker)
+        
+        let table = UITableView(frame: f, style: .grouped)
+        table.delegate = self
+        table.dataSource = self
+        table.showsVerticalScrollIndicator = false
+        table.separatorStyle = .none
+        table.bounces = false
+        table.backgroundColor = .white
+        
+        //for scrollable padding
+        table.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: self.frame.size.height / 2))
+        table.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: self.frame.size.height / 2))
+        if setHorizontal == true {
+            table.transform = CGAffineTransform(scaleX: 1, y: -1);
+        }
+
+        self.table = table
+        self.addSubview(table)
+    }
+    
+    func addIndicatorArrow() {
+        var f = self.frame
+        f.size.height = 60
+        f.origin.y = self.frame.size.height / 2 - f.size.height / 2
+        f.origin.x = 0
+        let pointer = ScalePointer(frame: f)
+        pointer.color = .blue
+        pointer.scScaleLable.text = scale
+        
+        var fnt = font
+        pointer.scScaleLable.font = fnt
+        pointer.valueLabel.text = "100"
+        fnt = font.withSize(font.pointSize * 2)
+        pointer.valueLabel.font = fnt
+        pointer.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        if setHorizontal == true {
+            pointer.rotateSubViews(angle: -90)
+        }
+        
+        self.pointer = pointer
+        self.addSubview(pointer)
     }
     
     //MARK:
-    //UIPickerViewDataSource
-    // returns the number of 'columns' to display.
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        pickerView.subviews.forEach({
-            $0.isHidden = $0.frame.height < 1.0 || $0.frame.height > 2
-        })
-        return 1
-    }
-   
+    //UITableViewDataSource
     // returns the # of rows in each component..
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return maxValue
-    }
-
-    //UIPickerViewDelegate
-    //static let indicatorView
-    public func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        var indexView : UIView!
-        if let v = view {
-            indexView = v
-        }else {
-            var w = pickerView.frame.size.width / 2
-            var h : CGFloat = 1
-            if (row % 5 == 0) {
-                w = pickerView.frame.size.width
-                h = 2
-            }
-            
-            let f = CGRect(x: pickerView.frame.size.width - w, y: CGFloat(0), width: w, height: h)
-            indexView = UIView(frame:f)
-            indexView.backgroundColor = color
-        }
-        return indexView
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return (range.length - range.location) / interval
     }
     
-    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return interval
+    }
+
+    //UITableViewDelegate
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let indicatorCellIdentifier = (indexPath.row % 5 == 0) ? "segmentIndicatorCellIdentifier"  : "indicatorCellIdentifier"
+        var cell : ScaleBarCell!
+        if let c = tableView.dequeueReusableCell(withIdentifier: indicatorCellIdentifier) as? ScaleBarCell {
+            cell = c
+        } else {
+            cell = ScaleBarCell(style: .default, reuseIdentifier: indicatorCellIdentifier)
+            var w = tableView.frame.size.width / 2
+            var h : CGFloat = 0.75
+            if (indexPath.row % 5 == 0) {
+                w = tableView.frame.size.width / 1.5
+                h = 1.5
+            }
+            let f = CGRect(x: tableView.frame.size.width - w, y: CGFloat(0), width: w, height: h)
+            cell.indicatorView.frame = f
+            cell.indicatorView.backgroundColor = color
+        }
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 8
     }
-
-    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-    }
-}
-
-//Arrow
-class Indicator: UIView {
-    let shapeLayer = CAShapeLayer()
-    var arrowColor:UIColor = .black {
-        didSet{
-            shapeLayer.fillColor = arrowColor.cgColor
-        }
-    }
     
-    init(origin: CGPoint, size: CGFloat ) {
-        super.init(frame: CGRect(x: origin.x, y: origin.y, width: size, height: size))
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-
-    override func draw(_ rect: CGRect) {
-        // Get size
-        let size = self.layer.frame.width
-
-        // Create path
-        let bezierPath = UIBezierPath()
-
-        // Draw points
-        let qSize = size/4
-
-        bezierPath.move(to: CGPoint(x: 0, y: qSize))
-        bezierPath.addLine(to: CGPoint(x: size, y: qSize))
-        bezierPath.addLine(to: CGPoint(x: size/2, y: qSize*3))
-        bezierPath.addLine(to: CGPoint(x: 0, y: qSize))
-        bezierPath.close()
-
-        // Mask to path
-        shapeLayer.path = bezierPath.cgPath
-       
-        if #available(iOS 12.0, *) {
-            self.layer.addSublayer (shapeLayer)
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        var hv : ScaleBarHeader!
+        if let v = tableView.dequeueReusableHeaderFooterView(withIdentifier: "scaleIndicatorHeader") as? ScaleBarHeader {
+            hv = v
         } else {
-            self.layer.mask = shapeLayer
+            hv = ScaleBarHeader(reuseIdentifier: "scaleIndicatorHeader")
+            var f = CGRect.zero
+            f.size.height = 32
+            f.size.width = tableView.frame.size.width
+            hv.frame = f
+            hv.textColor = textColor
+            hv.indicatorColor = color
+            hv.font = font
+            if setHorizontal == true {
+                hv.setHorizontal = setHorizontal
+            }
         }
+        let t = String(format: "%2d", ((section + 1) * interval) + range.location)
+        hv.label.text = "\(t)\(scale)"
+        return hv
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 32
+    }
+    
+    //Snap
+    public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let ch : CGFloat = 8
+        targetContentOffset.pointee.y = round(targetContentOffset.pointee.y / ch) * ch
+    }
+ 
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var cp  = self.table.center
+        cp.x = 20
+        cp.y += scrollView.contentOffset.y
+        print("cp = ", cp, "IndexPath: ", self.table.indexPathForRow(at: cp) ?? "Nohing", "scrollView.contentOffset.y = ", scrollView.contentOffset.y)
+        if let ip = self.table.indexPathForRow(at: cp) {
+            self.pointer.valueLabel.text = "\(ip.section * interval + ip.row)"
+        }
+        //print("cp = ", cp, "IndexPath: ", self.table.indexPathForRow(at: cp) ?? "Nohing")
     }
 }
